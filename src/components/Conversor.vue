da18350d21d808f157788a10819505efc7a0fe48
JulianGarzon22 <bryangarzon22@gmail.com>
2019-06-17 21:15:57 -0500

    Functional Conversor

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -3,83 +3,138 @@
     <v-layout row wrap>
       <v-flex md6>
         <v-textarea solo name="input-7-4" label="Copia y pega de tu tabla" v-model="table"></v-textarea>
+        <v-btn color="info" target="_blank" @click.prevent="compile()">
+          <span class="mr-2">COMPILAR</span>
+        </v-btn>
       </v-flex>
       <v-flex md3>
-        <v-hover>
-          <v-card
-            slot-scope="{ hover }"
-            :class="`elevation-${hover ? 12 : 2}`"
-            class="mx-auto"
-            width="250"
-          >
-            <v-card-title>
-              <div>
-                <span class="headline">C++ Codes</span>
-                <div class="d-flex">{{cppcodes}}</div>
-              </div>
-              <v-spacer></v-spacer>
-              <v-btn icon class="mr-0">
-                <v-icon>mdi-chevron-right</v-icon>
-              </v-btn>
-            </v-card-title>
-          </v-card>
-        </v-hover>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">C++ Codes</h3>
+              <div v-for="code in cppcodes" :key="code.id">{{code}}</div>
+            </div>
+          </v-card-title>
+        </v-card>
       </v-flex>
       <v-flex md3>
-        <v-hover>
-          <v-card
-            slot-scope="{ hover }"
-            :class="`elevation-${hover ? 12 : 2}`"
-            class="mx-auto"
-            width="250"
-          >
-            <v-card-title>
-              <div>
-                <span class="headline">Xilinx Codes</span>
-                <div class="d-flex">{{vhdlcodes}}</div>
-              </div>
-              <v-spacer></v-spacer>
-              <v-btn icon class="mr-0">
-                <v-icon>mdi-chevron-right</v-icon>
-              </v-btn>
-            </v-card-title>
-          </v-card>
-        </v-hover>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">VHDL Codes</h3>
+              <div v-for="codev in vhdlcodes" :key="codev.id">{{codev}}</div>
+            </div>
+          </v-card-title>
+        </v-card>
       </v-flex>
     </v-layout>
-    <v-btn color="info" target="_blank" @click.prevent="compile()">
-      <span class="mr-2">COMPILAR</span>
-    </v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    table: '',
+    isa: [
+      "load",
+      "store",
+      "add",
+      "sub",
+      "input",
+      "output",
+      "jpos",
+      "jneg",
+      "jz",
+      "jnz",
+      "jmp",
+      "halt"
+    ],
+    table: "",
     cppcodes: [],
     vhdlcodes: [],
+    rows: [],
+    dataarray: [],
+    n: 18
   }),
   methods: {
     compile: function() {
+      this.cppcodes = [];
       var splitlines = [];
-      var rows = [];
+      var cppisa = [];
+      var cppoperand = [];
       var lines = this.table.split("\n");
-      for (var i = 0; i < lines.length; i++) {
-        splitlines[i] = lines[i].split(" ");
-        if(splitlines[splitlines[i].length()] =!0) {
-          rows[i].operands = splitlines[splitlines.length()]
+
+      for (let i = 0; i < lines.length; i++) {
+        let z = i - 1;
+        splitlines[i] = lines[i].split("\t");
+        this.rows[i] = {
+          section: splitlines[i][0],
+          label: splitlines[i][1]
+            ? splitlines[i][1].toLowerCase()
+            : splitlines[i][1],
+          mnemo: splitlines[i][2]
+            ? splitlines[i][2].toLowerCase()
+            : splitlines[i][2],
+          operands: splitlines[i][3]
+            ? splitlines[i][3].toLowerCase()
+            : splitlines[i][3],
+          hexindex: z.toString(16)
+        };
+      }
+
+      for (let i = 0; i < this.rows.length; i++) {
+        if (this.isa.indexOf(this.rows[i].mnemo) == -1) {
+          cppisa[i] = "0";
         } else {
-          rows[i].operands = null;
-        }
-        var n = splitlines[i].length() - 1;
-        if(splitlines[n] == ""){
-          n = n-1;
-        } else {
-          rows[i].mnemo = splitlines[i]
+          cppisa[i] = this.isa.indexOf(this.rows[i].mnemo).toString(16);
         }
       }
-      console.log(columns);
+
+      this.n = this.rows
+        .map(function(e) {
+          return e.section;
+        })
+        .indexOf(".data");
+
+      let x = 0;
+      for (let j = this.n + 1; j < this.rows.length; j++) {
+        this.dataarray[x] = {
+          label: this.rows[j].label,
+          hexindex: x.toString(16)
+        };
+        x++;
+      }
+
+      for (let i = 0; i < this.rows.length; i++) {
+        if (this.rows[i].operands) {
+          let codeoperand = this.rows.find(
+            z => z.label == this.rows[i].operands
+          );
+          let dataoperand = this.dataarray.find(
+            z => z.label == this.rows[i].operands
+          );
+          if (codeoperand && dataoperand) {
+            cppoperand[i] = "0" + dataoperand.hexindex;
+          } else if (codeoperand && !dataoperand) {
+            cppoperand[i] = "0" + codeoperand.hexindex;
+          } else {
+            cppoperand[i] = "00";
+          }
+        } else {
+          cppoperand[i] = "00";
+        }
+
+        if (this.rows[i].mnemo) {
+          this.cppcodes.push(
+            "0x" + cppisa[i].toUpperCase() + cppoperand[i].toUpperCase() + ","
+          );
+        }
+
+        if (this.rows[i].mnemo) {
+          this.vhdlcodes.push(
+            'x"' + cppisa[i].toUpperCase() + cppoperand[i].toUpperCase()+ '"' + ","
+          );
+        }
+      }
     }
   }
 };
